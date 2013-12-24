8c574cee074ce3a84e4570ef416379e8813f5cd8
Alex-Werner <alex@werner.fr>
2013-12-24 13:48:47 +0100

    Added a new project for testing the Wrapper API

---
+++ SkyMovie/SkyMovieTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SkyMovieTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestConnectToIMDB()
+        {
+            //7d7ca9569a6728a3fee259e79fda3bff
+        }
+    }
+}
